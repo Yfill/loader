ff884fc2a0175b88a68406249a484f64e1c79749
Yfill <vps998@gmail.com>
2021-08-31 00:17:35 +0800

    feat: extend the method of judging objects

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -6,6 +6,8 @@ export const isString = (value: unknown) => getType(value) === 'string';
 
 export const isFunction = (value: unknown) => getType(value) === 'function';
 
+export const isObject = (value: unknown) => getType(value) === 'object';
+
 function check(fn: (value: unknown) => boolean, arg: unknown, typeName: string, errMes?: string) {
   if (!fn(arg)) throw new TypeError(errMes || `${arg} is not a ${typeName}`);
 }
@@ -27,6 +29,7 @@ export default {
   isArray,
   isString,
   isFunction,
+  isObject,
   assertionArray,
   assertionString,
   assertionFunction,
