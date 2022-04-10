2bbcd8507660de1587022413b529d0f0f31f59c6
Yfill <vps998@gmail.com>
2022-04-10 16:47:41 +0800

    feat: support for adding new targets to loaded

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import type { AsyncComponent } from 'vue/types/options';
 import type { PluginObject } from 'vue/types/plugin';
 import { JSDELIVR_REGISTRY, DEFAULT_OPT } from './constant';
 import { prefetchMulti } from './utils/prefetch';
+import { warn } from './utils/log';
 import {
   vueComponentLoad, loaded, load, unload,
 } from './utils/load';
@@ -30,7 +31,7 @@ export type Dep = Target
 export type LibAlias = [scriptAlias: string, styleAlias?: string, deps?: Dep[]]
 
 export interface LibAliasMap {
-  [key: string]: LibAlias
+  [key: string]: LibAlias;
 }
 
 export interface Options {
@@ -42,6 +43,7 @@ export interface Options {
   prefetch?: boolean,
   libAliasMap?: LibAliasMap
   loadedMap?: { [name: string]: unknown }
+
   [key: string]: unknown
 }
 
@@ -49,8 +51,12 @@ const initGlobalVariables = () => {
   function define(...arg: unknown[]) {
     const factory = arg.slice(-1)[0];
     const deps = [undefined, ...arg].slice(-2)[0];
-    setLoadedObj({ deps, factory });
+    setLoadedObj({
+      deps,
+      factory,
+    });
   }
+
   define.amd = {};
   window.define = define;
 };
@@ -67,7 +73,10 @@ const initDnsPrefetch = (options: Options) => {
 };
 
 const initPrefetch = (options: Options) => {
-  const { prefetch, libAliasMap } = options;
+  const {
+    prefetch,
+    libAliasMap,
+  } = options;
   if (!prefetch) return;
   const prefetchLibList = Object.keys(libAliasMap || {});
   prefetchMulti(options, <string[]>prefetchLibList);
@@ -76,42 +85,56 @@ const initPrefetch = (options: Options) => {
 const initSupportsPromiseDetection = () => {
   if (typeof Promise === 'undefined') throw new Error('Promise is a dependency of Loader, but Promise does not exist');
 };
+
 interface Loaded {
-  <T>(name: string): T
-  <T>(names: string[]): T[]
+  <T>(name: string): T;
+
+  <T>(names: string[]): T[];
 }
+
+interface Add {
+  (name: string, targetLoaded: unknown): void;
+}
+
 interface Load {
-  <T>(target: Target): Promise<T>
-  <T>(target: Target[]): Promise<T[]>
+  <T>(target: Target): Promise<T>;
+
+  <T>(target: Target[]): Promise<T[]>;
 }
 
 interface Unload {
-  (name: string): void
-  (names: string[]): void
+  (name: string): void;
+
+  (names: string[]): void;
 }
 
 interface VueComponentLoad {
-  (target: Target): AsyncComponent
+  (target: Target): AsyncComponent;
 }
 
 export interface Loader extends PluginObject<never>, Load {
-  options: Options
-  loaded: Loaded
-  load: Load
-  unload: Unload
-  vueComponentLoad: VueComponentLoad
+  options: Options;
+  loaded: Loaded;
+  add: Add;
+  load: Load;
+  unload: Unload;
+  vueComponentLoad: VueComponentLoad;
 }
 
 interface LoaderFactory {
-  (options?: Options): Loader
-  create(options?: Options): Loader
+  (options?: Options): Loader;
+
+  create(options?: Options): Loader;
 }
 
 initSupportsPromiseDetection();
 initGlobalVariables();
 
 function loaderFactory(options?: Options): Loader {
-  const { registry, libAliasMap } = options || {};
+  const {
+    registry,
+    libAliasMap,
+  } = options || {};
   const useJsdelivr = !registry || registry === JSDELIVR_REGISTRY;
   const loadedMap: { [key: string]: unknown } = isObject(options?.LoadedMap)
     ? { ...(<object>options?.LoadedMap) }
@@ -129,12 +152,14 @@ function loaderFactory(options?: Options): Loader {
   };
   initDnsPrefetch(opt);
   initPrefetch(opt);
+
   function handler<T>(target: Target): Promise<T>
   function handler<T>(targetList: Target[]): Promise<T[]>
   function handler<T>(arg: Target | Target[]): Promise<T | T[]> {
     if (arg instanceof Array) return load<T>(opt, arg);
     return load<T>(opt, arg);
   }
+
   function install(V: typeof Vue) {
     const { LoadedMap } = getLoadBase();
     LoadedMap.vue = V;
@@ -144,15 +169,26 @@ function loaderFactory(options?: Options): Loader {
       },
     });
   }
+
+  function add(name: string, targetLoaded: unknown) {
+    if (opt.loadedMap[name] || loaded(name)) {
+      warn(`${name} already exists`);
+      return;
+    }
+    opt.loadedMap[name] = targetLoaded;
+  }
+
   if (!opt.loadedMap.$loader) opt.loadedMap.$loader = handler;
   handler.options = opt;
   handler.install = install;
+  handler.add = add;
   handler.load = handler;
   handler.loaded = loaded;
   handler.unload = unload;
   handler.vueComponentLoad = (target: Target) => vueComponentLoad(opt, target);
   return handler;
 }
+
 loaderFactory.create = (options?: Options) => loaderFactory(options);
 
 const Loader: LoaderFactory = loaderFactory;
